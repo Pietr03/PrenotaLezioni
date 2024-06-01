43bfb642b7264ac2e118f326c724be31714ecd35
pietro <pietrocolosimosalvatore@gmail.com>
2024-06-01 17:56:23 +0200

    lezione sabato 01/06/2024

---
+++ scartati/script.js
@@ -0,0 +1,19 @@
+/*
+const path = window.location.pathname;
+const page = path.split("/").pop().replace(".html", '');
+
+// Spiegazione con aiuto della console
+console.log(path); // stampa l'intero indirizzo
+console.log(path.split("/")); // spezza l'indirizzo dopo ogni slash e li salva in un array
+console.log(path.split("/").pop()); // recupera l'ultimo elemento dell'array
+console.log(path.split("/").pop().replace(".html", "")); // sostituisce il pezzo ".html" con una stringa vuota, lasciando il nome puro della pagina
+
+const menuItem = document.querySelector(`#${page}`);
+if (menuItem) {
+  menuItem.classList.add("active");
+}
+*/
+nome = "ciao";
+nome = nome && false;
+
+console.log(nome);
